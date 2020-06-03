ceb64534004dd3e19884a8ca5214e29979090a74
Björn Grüning <bjoern@gruenings.eu>
2020-06-03 22:38:00 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,15 +7,15 @@ with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(
-    name='ENA_upload',
+    name='ena-upload-cli',
     version='0.1',
-    keywords=["pip", "ENA_upload", "cli"],
+    keywords=["pip", "ena-upload", "cli"],
     description='Command Line Interface to upload data to the European Nucleotide Archive',
     author="Dilmurat Yusuf",
     author_email="bjoern.gruening@gmail.com",
     long_description_content_type='text/markdown',
     long_description=long_description,
-    url="https://github.com/BackofenLab/ENA-upload-tool",
+    url="https://github.com/usegalaxy-eu/ena-upload-cli",
     packages=['.'],
     install_requires=[required],
     classifiers=[
@@ -24,7 +24,7 @@ setup(
     python_requires='>=2.7',
     entry_points={
         "console_scripts": [
-            "ENA_upload = ENA_upload:main"
+            "ena-upload = ena-upload:main"
         ]
     },
 )
