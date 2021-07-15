d2aa8d2dce22fe9d2cd3000d45b34ff20e48af8b
bedroesb <bedro@psb.vib-ugent.be>
2021-07-15 09:44:51 +0200

    version as module

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup, find_packages
-import re
+from ena_upload._version import __version__
 
 with open("README.md", 'r') as f:
     long_description = f.read()
@@ -7,19 +7,9 @@ with open("README.md", 'r') as f:
 with open('requirements.txt', 'r') as f:
     required = f.read().splitlines()
 
-PEP440_PATTERN = r"([1-9][0-9]*!)?(0|[1-9][0-9]*)(\.(0|[1-9][0-9]*))*((a|b|rc)(0|[1-9][0-9]*))?(\.post(0|[1-9][0-9]*))?(\.dev(0|[1-9][0-9]*))?"  # noqa
-
-with open('ena_upload/_version.py') as f:
-    v = f.read().strip()
-    m = re.match(r'^__version__ = "(' + PEP440_PATTERN + ')"$', v)
-    if not m:
-        msg = ('ena_upload/_version.py did not match pattern '
-                '__version__ = "0.1.2"  (see PEP440):\n') + v
-        raise Exception(msg)
-
 setup(
     name='ena-upload-cli',
-    version=m.group(1),
+    version=__version__,
     keywords=["pip", "ena-upload-cli", "cli", "ENA", "upload"],
     description='Command Line Interface to upload data to the European Nucleotide Archive',
     author="Dilmurat Yusuf",
