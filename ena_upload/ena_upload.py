ca4bdcac18ccc884db773f2c687db8e7477ce9b4
bedroesb <bedro@psb.vib-ugent.be>
2021-08-09 18:04:41 +0200

    adding columns

--- ena_upload/ena_upload.py
+++ ena_upload/ena_upload.py
@@ -14,6 +14,7 @@ import hashlib
 import ftplib
 import requests
 import uuid
+import numpy as np
 import re
 from genshi.template import TemplateLoader
 from lxml import etree
@@ -27,7 +28,7 @@ from ena_upload._version import __version__
 pd.options.mode.chained_assignment = None
 
 
-def create_dataframe(schema_tables):
+def create_dataframe(schema_tables, action):
     '''create pandas dataframe from the tables in schema_tables
        and return schema_dataframe
 
@@ -46,7 +47,14 @@ def create_dataframe(schema_tables):
 
     for schema, table in schema_tables.items():
         df = pd.read_csv(table, sep='\t', comment='#')
-
+        # checking for optional columns and if not present adding them
+        optional_columns = ['accession', 'submission_date', 'status']
+        for header in optional_columns:
+            if not header in df.columns:
+                if header == 'status':
+                    df[header] = action.lower()
+                else:
+                    df[header] = np.nan
         # status column contain action keywords
         # for xml rendering, keywords require uppercase
         # according to scheme definition of submission
@@ -496,7 +504,7 @@ def update_table(schema_dataframe, schema_targets, schema_update):
             if schema == 'sample':
                 dataframe.loc[index,
                               'taxon_id'] = targets.loc[index, 'taxon_id']
-            if schema == 'run':
+            elif schema == 'run':
                 # since index is set to alias
                 # then targets of run can have multiple rows with
                 # identical index because each row has a file
@@ -682,7 +690,7 @@ def main():
     schema_tables = collect_tables(args)
 
     # create dataframe from table
-    schema_dataframe = create_dataframe(schema_tables)
+    schema_dataframe = create_dataframe(schema_tables, action)
 
     # ? add a function to sanitize characters
     # ? print 'validate table for specific action'
