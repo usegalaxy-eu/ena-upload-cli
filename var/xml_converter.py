5fa806def8ad2b12248149ce9f56208614177fe1
bedroesb <bedro@psb.vib-ugent.be>
2021-12-01 11:00:16 +0100

    start for converter

--- var/xml_converter.py
+++ var/xml_converter.py
@@ -2,46 +2,57 @@ from lxml import etree
 from jinja2 import Environment, FileSystemLoader
 import requests
 
-# Gathering all checklist ID's
-session = requests.Session()
-session.trust_env = False
-r = session.get('https://www.ebi.ac.uk/ena/browser/api/summary/ERC000001-ERC999999')
 
-for response_object in r.json()['summaries']:
-    checklist = response_object['accession']
-    print(f"Parsing {checklist}")
-    # Getting the xml checklist from ENA
-    url = f"https://www.ebi.ac.uk/ena/browser/api/xml/{checklist}?download=true"
-    r = requests.get(url, allow_redirects=True)
+def fetching_checklists():
+    # Gathering all checklist ID's
+    session = requests.Session()
+    session.trust_env = False
+    r = session.get('https://www.ebi.ac.uk/ena/browser/api/summary/ERC000001-ERC999999')
+    return r.json()['summaries']
 
-    # Dictionary that will contain all attributes needed
-    xml_tree = {}
+    
 
-    # Loading templates directory
-    file_loader = FileSystemLoader('ena_upload/templates')
-    env = Environment(loader=file_loader)
+def main():
+    for response_object in fetching_checklists():
+        checklist = response_object['accession']
+        print(f"Parsing {checklist}")
+        # Getting the xml checklist from ENA
+        url = f"https://www.ebi.ac.uk/ena/browser/api/xml/{checklist}?download=true"
+        r = requests.get(url, allow_redirects=True)
 
-    # Parsing XML
-    root = etree.fromstring(r.content)
+        # Dictionary that will contain all attributes needed
+        xml_tree = {}
 
-    # Looping over all fields and storing their name and cardinality
-    for attribute in root.iter('FIELD'):
-        name = ''
-        cardinality = ''
-        for sub_attr in attribute:
-            if sub_attr.tag == 'NAME':
-                name = sub_attr.text
-            elif sub_attr.tag == 'MANDATORY':
-                cardinality = sub_attr.text
-        xml_tree[name] = cardinality
+        # Loading templates directory
+        file_loader = FileSystemLoader('ena_upload/templates')
+        env = Environment(loader=file_loader)
 
-    # Loading the xml jinja2 template for samples
-    t = env.get_template('ENA_template_samples.xml')
+        # Parsing XML
+        root = etree.fromstring(r.content)
 
-    # Render template with values from the ENA xml
-    output_from_parsed_template = t.render(attributes=xml_tree)
+        # Looping over all fields and storing their name and cardinality
+        for attribute in root.iter('FIELD'):
+            name = ''
+            cardinality = ''
+            for sub_attr in attribute:
+                if sub_attr.tag == 'NAME':
+                    name = sub_attr.text
+                elif sub_attr.tag == 'MANDATORY':
+                    cardinality = sub_attr.text
+            xml_tree[name] = cardinality
 
-    # Saving new xml template file
-    with open(f"ena_upload/templates/ENA_template_samples_{checklist}.xml", "w") as fh:
-        fh.write(output_from_parsed_template)
+        # Loading the xml jinja2 template for samples
+        t = env.get_template('ENA_template_samples.xml')
+
+        # Render template with values from the ENA xml
+        output_from_parsed_template = t.render(attributes=xml_tree)
+
+        # Saving new xml template file
+        with open(f"ena_upload/templates/ENA_template_samples_{checklist}.xml", "w") as fh:
+            fh.write(output_from_parsed_template)
+
+
+if __name__ == "__main__":
+
+    main()
 
