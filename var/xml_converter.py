24f43183693de4adf7e9062e73fb6061411944a2
bedroesb <bedro@psb.vib-ugent.be>
2021-12-17 12:25:35 +0100

    better requests

--- var/xml_converter.py
+++ var/xml_converter.py
@@ -1,16 +1,37 @@
 from lxml import etree
 from jinja2 import Environment, FileSystemLoader
 import requests
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
+import time
 
+def fetch_object(url):
+    """
+    Fetch single BrAPI object by path
+    """
+    print('  GET ' + url)
+    session = requests.Session()
+    retry = Retry(connect=3, backoff_factor=15)
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+    r = session.get(url)
+    # Covering internal server errors by retrying one more time
+    if r.status_code == 500:
+        time.sleep(5)
+        r = requests.get(url, allow_redirects=True)
+    elif r.status_code != 200:
+        print(f"Problem with request: {str(r)}")
+        raise RuntimeError("Non-200 status code")
+    return r
 
 def fetching_checklists():
     # Gathering all checklist ID's
     session = requests.Session()
     session.trust_env = False
-    r = session.get('https://www.ebi.ac.uk/ena/browser/api/summary/ERC000001-ERC999999')
-    return r.json()['summaries']
+    response = fetch_object('https://www.ebi.ac.uk/ena/browser/api/summary/ERC000001-ERC999999')
+    return response.json()['summaries']
 
-    
 
 def main():
     for response_object in fetching_checklists():
@@ -18,17 +39,17 @@ def main():
         print(f"Parsing {checklist}")
         # Getting the xml checklist from ENA
         url = f"https://www.ebi.ac.uk/ena/browser/api/xml/{checklist}?download=true"
-        r = requests.get(url, allow_redirects=True)
+        response = fetch_object(url)
 
         # Dictionary that will contain all attributes needed
         xml_tree = {}
 
         # Loading templates directory
-        file_loader = FileSystemLoader('ena_upload/templates')
+        file_loader = FileSystemLoader('ena_upload/templates/jinja_templates')
         env = Environment(loader=file_loader)
 
         # Parsing XML
-        root = etree.fromstring(r.content)
+        root = etree.fromstring(response.content)
 
         # Looping over all fields and storing their name and cardinality
         for attribute in root.iter('FIELD'):
