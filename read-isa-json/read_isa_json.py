72e3c56d362e6911bf0be2111d14159204e063fc
Kevin De Pelseneer <kevin.depelseneer@psb.ugent.be>
2023-09-22 18:04:51 +0200

    Update test file.

--- read-isa-json/read_isa_json.py
+++ read-isa-json/read_isa_json.py
@@ -1,15 +1,11 @@
 import json
-from isa_objects import isa_study
+from isa_objects.isa_study import IsaStudy
 
 from rich import print_json
 
 # Read json file
-isa_json_file = open('test_isa_json_files/test_local_instance.json')
+isa_json_file = open("read-isa-json/test_isa_json_files/test_local_instance.json")
 isa_json = json.load(isa_json_file)
-# print_json(data = isa_json)
 
-# Extracting the Study information
-studies = isa_json['studies']
-print_json(data=studies)
-
-# study_information = isa_study.IsaStudy.from_isa_json(isa_json)
+study_information = IsaStudy.from_isa_json(isa_json)
+print(study_information)
