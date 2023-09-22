572c49bac934e73599beae0ed3dbaec1acd7a9c9
Kevin De Pelseneer <kevin.depelseneer@psb.ugent.be>
2023-09-22 18:04:11 +0200

    Updated isa_study.py:
    - Initiate IsaStudy directly from isa JSON file
    - Add typing system
    - Add Dict validation before creating the IsaStudy object

--- read-isa-json/isa_objects/isa_study.py
+++ read-isa-json/isa_objects/isa_study.py
@@ -1,13 +1,45 @@
+from typing import List, Optional, Dict
+
+
+def study_publication_ids(publication_isa_json) -> List[int]:
+    return [pub["id"] for pub in publication_isa_json]
+
+
+def validate_isa_json(isa_json: Dict, key: str) -> None:
+    if not key in isa_json.keys():
+        raise KeyError(f"{key} was not found in the provided ISA JSON.")
+
+
 class IsaStudy:
     def __init__(
-        self, alias, title, study_type, study_abstract, new_study_type=None
+        self,
+        alias: str,
+        title: str,
+        study_type: str,
+        study_abstract: str,
+        new_study_type=Optional[str],
+        pubmed_id=Optional[List[int]],
     ) -> None:
         self.alias = alias
         self.title = title
         self.study_type = study_type
         self.new_study_type = new_study_type
         self.study_abstract = study_abstract
+        self.pubmed_id = pubmed_id
 
-    @classmethod
     def from_isa_json(isa_json):
-        pass
+        mandatory_keys = ["title", "description", "publications"]
+        [validate_isa_json(isa_json, key) for key in mandatory_keys]
+
+        return [
+            IsaStudy(
+                alias="",  # TODO: Add SEEK URL of Study
+                title=study["title"],
+                study_type="",  # TODO: Replace by Custom metadata of the Assay level
+                study_abstract=study["description"],
+                pubmed_id=study_publication_ids(
+                    publication_isa_json=study["publications"]
+                ),
+            )
+            for study in isa_json["studies"]
+        ]
