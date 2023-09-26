6493e4e5f7084e85994cf1ae353766ea931438d1
Kevin De Pelseneer <kevin.depelseneer@psb.ugent.be>
2023-09-26 20:14:56 +0200

    Implementation of the Ena Samples functionality

--- read-isa-json/ena_objects/ena_sample.py
+++ read-isa-json/ena_objects/ena_sample.py
@@ -1,4 +1,53 @@
+import re
 from typing import List, Dict
+
+from pandas import DataFrame
+
+
+def study_characteristic_category_name(study_dict: Dict, id: str) -> Dict:
+    char_cat_dicts = [
+        {"id": cc["@id"], "name": cc["characteristicType"]["annotationValue"]}
+        for cc in study_dict["characteristicCategories"]
+    ]
+
+    for ccd in char_cat_dicts:
+        if ccd["id"] == id:
+            return ccd["name"]
+
+
+def fetch_characteristics(sample_dict: Dict, study_dict: Dict) -> List[Dict]:
+    return [
+        {
+            "category_id": char["category"]["@id"],
+            "category_name": study_characteristic_category_name(
+                study_dict, char["category"]["@id"]
+            ),
+            "value": char["value"]["annotationValue"],
+        }
+        for char in sample_dict["characteristics"]
+    ]
+
+
+def associated_source(sample_dict: Dict, study_dict: Dict) -> List[str]:
+    sample_id = sample_dict["@id"]
+    for process in study_dict["processSequence"]:
+        input_ids = [input["@id"] for input in process["inputs"]]
+        output_ids = [output["@id"] for output in process["outputs"]]
+        if sample_id in output_ids:
+            return input_ids
+
+
+def associated_source_characteristics(sources_data: Dict, ids: List[str]) -> Dict:
+    for sd in sources_data:
+        if sd["id"] in ids:
+            return sd["characteristics"]
+
+
+def sample_alias(id: str):
+    prefix = "https://datahub.elixir-belgium.org/samples/"  # TODO: Replace by something less hard-coded
+
+    sample_id = re.split("/", id)[1]
+    return prefix + sample_id
 
 
 class EnaSample:
@@ -6,14 +55,56 @@ class EnaSample:
     Generates an Sample object, compliant to the requirements of ENA
     """
 
-    def __init__(
-        self,
-        alias: str = "Sample alias",
-        status: str = "sample status",
-        taxon_id: int = "sample_taon_id",
-        sample_description: str = "sample_description",
-    ) -> None:
+    def __init__(self, characteristics: Dict, alias: str) -> None:
         self.alias = alias
-        self.status = status
-        self.taxon_id = taxon_id
-        self.sample_description = sample_description
+        self.characteristics = characteristics
+
+    def __dict__(self):
+        return {
+            "alias": self.alias,
+            "characteristics": self.characteristics,
+        }
+
+    def from_study_dict(study_dict: Dict) -> None:
+        sources_data = [
+            {
+                "id": source["@id"],
+                "name": source["name"],
+                "characteristics": fetch_characteristics(source, study_dict),
+            }
+            for source in study_dict["materials"]["sources"]
+        ]
+
+        samples_data = [
+            {
+                "id": sample["@id"],
+                "name": sample["name"],
+                "characteristics": fetch_characteristics(sample, study_dict),
+                "source": associated_source(sample, study_dict),
+            }
+            for sample in study_dict["materials"]["samples"]
+        ]
+
+        for sd in samples_data:
+            for sc in associated_source_characteristics(sources_data, sd["source"]):
+                sd["characteristics"].append(sc)
+
+        return [
+            EnaSample(
+                alias=sample_alias(sd["id"]),
+                characteristics=sd["characteristics"],
+            )
+            for sd in samples_data
+        ]
+
+
+def export_samples_to_dataframe(samples: List[EnaSample]):
+    flat_dicts = []
+    for sample in samples:
+        sample_dict = sample.__dict__()
+        characteristics = sample_dict.pop("characteristics")
+        for char in characteristics:
+            sample_dict.update({char["category_name"]: char["value"]})
+        flat_dicts.append(sample_dict)
+
+    return DataFrame.from_dict(flat_dicts)
--- read-isa-json/read_isa_json.py
+++ read-isa-json/read_isa_json.py
@@ -1,10 +1,22 @@
 import json
+
+from ena_objects.ena_sample import EnaSample, export_samples_to_dataframe
+
 from ena_objects.ena_study import EnaStudy
 
 # Read json file
-isa_json_file = open("read-isa-json/test_isa_json_files/test_local_instance.json")
+isa_json_file = open(
+    "read-isa-json/test_isa_json_files/isa_json_test_investigation.json"
+)
 isa_json = json.load(isa_json_file)
 
 studies = EnaStudy.from_isa_json(isa_json)
 study_dfs = [study.to_dataframe() for study in studies]
 print(study_dfs[0])
+
+study_dict = isa_json["studies"][0]
+
+samples = EnaSample.from_study_dict(study_dict)
+samples_df = export_samples_to_dataframe(samples)
+print(samples_df)
+print("Done!")
