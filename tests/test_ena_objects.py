138c596be51e992008710ba9f591fe6d64ea5973
Kevin De Pelseneer <kevin.depelseneer@psb.ugent.be>
2023-09-27 11:58:48 +0200

    Add test for reading an ISA JSON and producing studies

--- tests/test_ena_objects.py
+++ tests/test_ena_objects.py
@@ -1,5 +1,11 @@
+import os
 import pytest
+import json
 from ena_objects.ena_study import EnaStudy, EnaSample, EnaExperiment, EnaRun
+
+test_isa_jsonfile = open("tests/test_data/isa_json_test_investigation.json")
+
+test_isa_json = json.load(test_isa_jsonfile)
 
 
 class TestEnaStudy:
@@ -16,6 +22,21 @@ class TestEnaStudy:
             KeyError, match="description was not found in the provided ISA JSON."
         ):
             EnaStudy.from_isa_json(bad_dict)
+
+    def test_ena_study_creation(self):
+        studies = EnaStudy.from_isa_json(test_isa_json)
+
+        assert len(studies) == 1
+        assert studies[0].__dict__() == dict(
+            {
+                "alias": "https://datahub.elixir-belgium.org/studies/2",
+                "title": "ENA Upload Study",
+                "study_type": "",
+                "study_abstract": "",
+                "new_study_type": None,
+                "pubmed_id": [1],
+            }
+        )
 
 
 class TestEnaSample:
