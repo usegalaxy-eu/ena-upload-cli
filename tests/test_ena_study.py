2ff3ca1738a63f41a0f2bbad5c49ce395acb504b
Kevin De Pelseneer <kevin.depelseneer@psb.ugent.be>
2023-09-27 09:04:55 +0200

    Add Test Class for Ena Study

---
+++ tests/test_ena_study.py
@@ -0,0 +1,18 @@
+import pytest
+from read_isa_json.ena_objects.ena_study import EnaStudy
+
+
+class TestEnaStudy:
+    """Test class for Ena Study objects"""
+
+    def test_should_raise_key_error(self):
+        bad_dict = {
+            "title": "My Title",
+            "study_description": "Should 'description'",
+            "publications": None,
+        }
+
+        with pytest.raises(
+            KeyError, match="description was not found in the provided ISA JSON."
+        ):
+            EnaStudy.from_isa_json(bad_dict)
